bf92a4d750d5bfbad5c230252a4e33afedcb6aaa
Evandro Pimentel de Areis <evandro-96@live.com>
2016-04-20 21:12:06 -0300

    Criando classe SqlGen

---
+++ src/SqlGen.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public abstract class SqlGen {
+	
+	protected abstract String getCreateTable(Object obj);
+	
+	protected abstract String getDropTable(Object obj);
+	
+	protected abstract PreparedStatement getSqlInsert(Connection con, Object obj);
+	protected abstract PreparedStatement getSqlSelectAll(Connection con, Object obj);
+	protected abstract PreparedStatement getSqlSelectById(Connection con, Object obj);
+	protected abstract PreparedStatement getSqlUpdateById(Connection con, Object obj);
+	protected abstract PreparedStatement getSqlDeleteById(Connection con, Object obj);
+
+}

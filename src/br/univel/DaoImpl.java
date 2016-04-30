76a828a5a11ced7721743f59a5ee0c68db1a745b
Evandro Pimentel de Areis <evandro-96@live.com>
2016-04-30 16:39:53 -0300

    Criando e alterando a Classe Main

---
+++ src/br/univel/DaoImpl.java
@@ -0,0 +1,180 @@
+package br.univel;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.univel.enums.EstadoCivil;
+import br.univel.interfaceseclassesabstratas.Dao;
+
+public class DaoImpl implements Dao<Cliente, Integer> {
+
+
+	private Connection con = null;
+	
+	
+	public Connection getCon() {
+		return con;
+	}
+
+	public void setCon(Connection con) {
+		this.con = con;
+	}
+
+	@Override
+	public void salvar(Cliente t) {
+		SqlGenImpl gerador = new SqlGenImpl();
+		
+		try {
+
+			PreparedStatement ps = gerador.getSqlInsert(con, t);
+			ps.setInt(1, t.getId());
+			ps.setString(2, t.getNome());
+			ps.setString(3, t.getEndereco());
+			ps.setString(4, t.getTelefone());
+			ps.setInt(5, t.getEstadoCivil().ordinal());
+			
+			ps.executeUpdate();
+			ps.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}			
+	}
+	@Override
+	public Cliente buscar(Integer k) {
+		SqlGenImpl gerador = new SqlGenImpl();
+		Cliente c = new Cliente();
+				
+		try {
+
+			PreparedStatement ps = gerador.getSqlSelectById(con, new Cliente());
+			ps.setInt(1, k);
+			ResultSet resultados = ps.executeQuery();
+			
+			while (resultados.next()) {
+				c.setId(resultados.getInt("cli_codigo"));
+				c.setNome(resultados.getString("cli_nome"));
+				c.setEndereco(resultados.getString("cli_endereco"));
+				c.setTelefone(resultados.getString("cli_fone"));
+				c.setEstadoCivil(EstadoCivil.getPorCodigo(resultados.getInt("cli_estcivil")));
+			}			
+			
+			ps.close();
+			resultados.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}				
+		
+		return c;
+	}
+
+	@Override
+	public void atualizar(Cliente t) {
+		SqlGenImpl gerador = new SqlGenImpl();
+			
+		try {
+
+			PreparedStatement ps = gerador.getSqlUpdateById(con, t);
+			ps.setString(1, t.getNome());
+			ps.setString(2, t.getEndereco());
+			ps.setString(3, t.getTelefone());
+			ps.setInt(4, t.getEstadoCivil().ordinal());
+			ps.setInt(5, t.getId());
+			
+			ps.executeUpdate();
+			ps.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}		
+	}
+
+	@Override
+	public void excluir(Integer k) {
+		SqlGenImpl gerador = new SqlGenImpl();
+				
+		try {
+
+			PreparedStatement ps = gerador.getSqlDeleteById(con, new Cliente());
+			ps.setInt(1, k);
+			ps.executeUpdate();
+			ps.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}		
+	}
+
+	@Override
+	public List<Cliente> listarTodos() {
+		SqlGenImpl gerador = new SqlGenImpl();
+		List<Cliente> listaCliente = new ArrayList<Cliente>();
+		
+		try {
+
+			PreparedStatement ps = gerador.getSqlSelectAll(con, new Cliente());
+			ResultSet resultados = ps.executeQuery();
+			
+			while (resultados.next()) {
+				
+				Cliente c = new Cliente();
+				c.setId(resultados.getInt("cli_codigo"));
+				c.setNome(resultados.getString("cli_nome"));
+				c.setEndereco(resultados.getString("cli_endereco"));
+				c.setTelefone(resultados.getString("cli_fone"));
+				c.setEstadoCivil(EstadoCivil.getPorCodigo(resultados.getInt("cli_estcivil")));
+				
+				listaCliente.add(c);
+			}			
+			
+			ps.close();
+			resultados.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}				
+		
+		return listaCliente;		
+	}
+
+	public void criarTabela(Cliente t){
+		SqlGenImpl gerador = new SqlGenImpl();
+		
+		try {
+			String sql = gerador.getCreateTable(con, t);	
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.executeUpdate();
+			ps.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}			
+		
+	}
+	
+	public void apagarTabela(Cliente t){
+		SqlGenImpl gerador = new SqlGenImpl();
+				
+		try {
+			String sql = gerador.getDropTable(con, t);	
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.executeUpdate();
+			ps.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}			
+		
+	}	
+	
+	
+}
---
+++ src/br/univel/Main.java
@@ -0,0 +1,93 @@
+package br.univel;
+
+import java.sql.SQLException;
+
+import br.univel.enums.EstadoCivil;
+
+public class Main {
+
+	public Main(){
+		//instancia dos clientes
+		Cliente c1 = new Cliente();
+		c1.setId(1);
+		c1.setNome("Evandro");
+		c1.setTelefone("99999999");
+		c1.setEstadoCivil(EstadoCivil.SOLTEIRO);
+		c1.setEndereco("Rua Da champa");
+		
+		Cliente c2 = new Cliente();
+		c2.setId(2);
+		c2.setNome("Tonho");
+		c2.setTelefone("989898989");
+		c2.setEstadoCivil(EstadoCivil.SOLTEIRO);
+		c2.setEndereco("Rua asa");
+		
+		Conexao conexao = new Conexao();
+		
+		
+		//instancia Dao
+		DaoImpl d = new DaoImpl();
+		
+		//seta conexão
+		try {
+			d.setCon(conexao.abrirConexao());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		//chamar métodos
+		System.out.println("apagarTabela\n");
+		d.apagarTabela(c1);
+		
+		System.out.println("criarTabela\n");
+		d.criarTabela(c1);
+		
+		System.out.println("inserir objeto 1\n");
+		d.salvar(c1);
+		
+		System.out.println("inserir objeto 2\n");
+		d.salvar(c2);
+		
+		System.out.println("listarTodos");
+		for(Cliente c : d.listarTodos()){
+			System.out.println(c.getId() + " - " + c.getNome() + " - " + c.getEndereco() + " - " + 
+							   c.getTelefone() + " - " + c.getEstadoCivil().toString());			
+		}
+		
+		
+		System.out.println("\nbuscar objeto 1");
+		Cliente c4 = new Cliente();
+		c4 = d.buscar(c1.getId());		
+		System.out.println(c4.getId() + " - " + c4.getNome() + " - " + c4.getEndereco() + " - " + 
+						   c4.getTelefone() + " - " + c4.getEstadoCivil().toString());			
+
+		
+		System.out.println("\nalterar objeto 2\n");
+		c2.setEstadoCivil(EstadoCivil.CASADO);
+		d.atualizar(c2);
+		
+		System.out.println("listarTodos");		
+		for(Cliente c : d.listarTodos()){
+			System.out.println(c.getId() + " - " + c.getNome() + " - " + c.getEndereco() + " - " + 
+							   c.getTelefone() + " - " + c.getEstadoCivil().toString());			
+		}
+		
+		
+		//fecha a conexão
+		d.setCon(null);
+		try {
+			conexao.fecharConexao();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		new Main();
+	}
+}

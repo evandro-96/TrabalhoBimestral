d80ca21fad8f5f173830331e1714efd5cb36bbfa
Evandro Pimentel de Areis <evandro-96@live.com>
2016-04-30 17:45:06 -0300

    Alterando classe Conexao

--- src/br/univel/Conexao.java
+++ src/br/univel/Conexao.java
@@ -1,12 +1,32 @@
 package br.univel;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexao {
 
-	public Connection abrirConexao() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	private Connection con = null;		
 
+	public Connection abrirConexao() throws SQLException {
+
+		String url = "jdbc:h2:~/test";
+		String user = "sa";
+		String pass = "sa";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		con = DriverManager.getConnection(url, user, pass);
+	
+		return con;
+	}	
+	
+	public void fecharConexao() throws SQLException {
+		con.close();
+	}
 }

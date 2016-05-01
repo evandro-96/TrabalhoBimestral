7e1fd6274fe5779d877dea57202ef7539ad2e476
Evandro Pimentel de Areis <evandro-96@live.com>
2016-04-30 22:42:20 -0300

    Criando classe Acoes

---
+++ src/br/univel/interfacesclassesabstratas/Acoes.java
@@ -0,0 +1,12 @@
+package br.univel.interfacesclassesabstratas;
+
+import java.util.List;
+
+public interface Acoes<T,K> {
+	
+	public void salvar(T t);
+	public T buscar(K k);
+	public void atualizar(T t);
+	public void excluir(K k);
+	public List<T> listarTodos();
+}
